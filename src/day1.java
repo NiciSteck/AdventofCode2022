9016fd73a6123b4472c20085378ddd7a0b238145
NiciSteck <nic2000.steck@gmail.com>
2022-12-02 13:49:35 +0100

    messing with file structure

--- src/day1.java
+++ src/day1.java
@@ -6,29 +6,26 @@ public class day1 {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("one");
         Scanner s = new Scanner(file);
-        int fst = 0;
-        int snd = 0;
-        int trd = 0;
-        int curr = 0;
+        int fst, snd, trd, curr;
+        fst = snd = trd = curr = 0;
         while (s.hasNextLine()) {
             String line = s.nextLine();
             if (line.equals("")) {
                 if (curr > fst) {
-                    trd=snd;
-                    snd=fst;
+                    trd = snd;
+                    snd = fst;
                     fst = curr;
-                } else if (curr>snd) {
-                    trd=snd;
+                } else if (curr > snd) {
+                    trd = snd;
                     snd = curr;
-                } else if (curr>trd) {
+                } else if (curr > trd) {
                     trd = curr;
                 }
                 curr = 0;
             } else {
                 curr += Integer.parseInt(line);
-
             }
         }
-        System.out.println(fst+snd+trd);
+        System.out.println(fst + snd + trd);
     }
 }
