bcf0987f056b6557e41497fd9c669a0e7557d17d
Nicolas <nic2000.steck@gmail.com>
2022-12-02 13:31:28 +0100

    Initial commit

---
+++ untitled/src/day1.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("one");
+        Scanner s = new Scanner(file);
+        int fst = 0;
+        int snd = 0;
+        int trd = 0;
+        int curr = 0;
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            if (line.equals("")) {
+                System.out.println(curr);
+                if (curr > fst) {
+                    trd=snd;
+                    snd=fst;
+                    fst = curr;
+                } else if (curr>snd) {
+                    trd=snd;
+                    snd = curr;
+                } else if (curr>trd) {
+                    trd = curr;
+                }
+                curr = 0;
+            } else {
+                curr += Integer.parseInt(line);
+
+            }
+        }
+        System.out.println(fst);
+        System.out.println(snd);
+        System.out.println(trd);
+        System.out.println(fst+snd+trd);
+    }
+}
