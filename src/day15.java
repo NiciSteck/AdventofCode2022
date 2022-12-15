89bdce5c08c52672ce565d0a9eef48fb28a52a22
Nicolas <nic2000.steck@gmail.com>
2022-12-15 17:27:37 +0100

    bruh

--- src/day15.java
+++ src/day15.java
@@ -2,50 +2,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class day15 {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner s = new Scanner(new File("fifteen"));
-        int maxx = 0;
-        int minx = 0;
-        int row = 10;
+        int row = 2000000;
         HashMap<ArrayList<Integer>, Integer> beaconDist = new HashMap<ArrayList<Integer>, Integer>();
+        HashSet<ArrayList<Integer>> beacons = new HashSet<ArrayList<Integer>>();
 
         while (s.hasNextLine()) {
             String[] line = s.nextLine().split(":");
             String[] sensor = line[0].split(",");
             String[] beacon = line[1].split(",");
 
-            ArrayList<Integer> sensCoords = createList(Integer.parseInt(sensor[0].replaceAll("[^0-9]", "")),
-                    Integer.parseInt(sensor[1].replaceAll("[^0-9]", "")));
+            ArrayList<Integer> sensCoords = createList(Integer.parseInt(sensor[0].replaceAll("[^0-9-]", "")),
+                    Integer.parseInt(sensor[1].replaceAll("[^0-9-]", "")));
 
-            int beaconx = Integer.parseInt(beacon[0].replaceAll("[^0-9]", ""));
-            int beacony = Integer.parseInt(beacon[1].replaceAll("[^0-9]", ""));
+            ArrayList<Integer> beacCoords = createList(Integer.parseInt(beacon[0].replaceAll("[^0-9-]", "")),
+                    Integer.parseInt(beacon[1].replaceAll("[^0-9-]", "")));
+            beacons.add(beacCoords);
 
-            int manhattan = manhattan(sensCoords.get(0), sensCoords.get(1), beaconx, beacony);
+            int manhattan = manhattan(sensCoords, beacCoords);
             beaconDist.put(sensCoords, manhattan);
 
-            if (sensCoords.get(0) + manhattan > maxx) maxx = sensCoords.get(0) + manhattan;
-            if (sensCoords.get(0) - manhattan < minx) minx = sensCoords.get(0) - manhattan;
         }
 
-        int result = 0;
+        HashSet<Integer> notABeacon = new HashSet<>();
 
-        for (int x = minx; x < maxx; x++) {
-            int visited = 0; //1 if visited, 0 if not
-            for (ArrayList<Integer> currSens:beaconDist.keySet()) {
-                int pointToSens = manhattan(currSens.get(0),currSens.get(1),x,row);
-                if(pointToSens<=beaconDist.get(currSens)) visited = 1;
+        for (ArrayList<Integer> currSens:beaconDist.keySet()
+             ) {
+            int mandiff = beaconDist.get(currSens)-Math.abs(row-currSens.get(1));
+            if(mandiff>=0){
+                for (int i = currSens.get(0)-mandiff; i <= currSens.get(0)+mandiff; i++) {
+                    ArrayList<Integer> point = createList(i,row);
+                    if(!beacons.contains(point)){
+                        notABeacon.add(i);
+                    }
+                }
             }
-            result += visited;
         }
-        System.out.println(result);
+
+        System.out.println(notABeacon.size());
 
     }
 
-    public static int manhattan(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    public static int manhattan(ArrayList<Integer> p1, ArrayList<Integer> p2) {
+        return Math.abs(p1.get(0) - p2.get(0)) + Math.abs(p1.get(1) - p2.get(1));
     }
 
     public static ArrayList<Integer> createList(int fst, int snd) {
